660eab695af73283eb6ffacaccfe533e2802091b
nhoegee <nigel@competa.com>
2016-10-18 11:49:23 +0200

    Fourth push.

--- resources/assets/js/slider.js
+++ resources/assets/js/slider.js
@@ -9,22 +9,6 @@ var slider = ( function (document) {
     var _previousSlide = _sliderContainer.querySelector( 'button[name=previous_slide]' );
     var _nextSlide = _sliderContainer.querySelector( 'button[name=next_slide]' );
 
-    var _queryAll = function ( container, element ) {
-      return Array.prototype.slice.call( container.querySelectorAll( element ));
-    };
-
-    var _addClass = function ( elements, c ) {
-      elements.forEach(function(element) {
-        element.classList.add( c );
-      });
-    }
-
-    var _removeClass = function ( elements, c ) {
-      elements.forEach( function ( element ) {
-        element.classList.remove( c );
-      });
-    };
-
     var _next = function ( elements ) {
       var next = [];
 
@@ -41,30 +25,29 @@ var slider = ( function (document) {
       }
     };
 
-    // TODO: Fix _setStyle.
-    var _setStyle = function ( elements, property, value ) {
-      elements.forEach(function(element) {
-        element.style.property = value;
-      });
-    }
-
     var _slide = function ( e ) {
       e.preventDefault();
 
-      var slides = _queryAll( _slider, '.slide.is-previous' );
-      _removeClass(slides, 'is-previous');
+      var slides = Array.prototype.slice.call( _slider.querySelectorAll( '.slide.is-reference' ));
+      slides.forEach( function ( element ) {
+        element.classList.remove( 'is-reference' );
+      });
 
-      var newSlide = _next( slides );
-      _addClass(newSlide, 'is-previous');
+      var newSlides = _next( slides );
+      newSlides.forEach(function( element ) {
+        element.classList.add( 'is-reference' );
+        element.style.order = 1;
+      });
 
-      var i = j;
-      var j;
       var length = _slides.length;
       var results = [];
-      for (i = j = 2; 2 <= length ? j <= length : j >= length; i = 2 <= length ? j++ : j--) {
-        newSlide = _next(newSlide);
-        _setStyle( newSlide, order, i);
-        results.push(newSlide);
+
+      for (var i = 2; i <= length; i++) {
+        newSlides = _next( newSlides );
+        newSlides.forEach( function( element ) {
+          element.style.order = i;
+        });
+        results.push( newSlides );
       }
       return results;
     };
