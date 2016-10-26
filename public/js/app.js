310744e792dc29f4a1f1b6d5f17ec6252a764fa3
nhoegee <nigel@competa.com>
2016-10-26 11:53:23 +0200

    Sixth push.

--- public/js/app.js
+++ public/js/app.js
@@ -1,87 +1,89 @@
-var slider = ( function (document) {
+var slider = (function (document) {
 
   'use strict';
 
-  document.addEventListener( 'DOMContentLoaded', function () {
-    var _sliderContainer = document.querySelector( '.slider-container' );
-    var _slider = _sliderContainer.querySelector( '.slider' );
+  document.addEventListener('DOMContentLoaded', function () {
+    var _sliderContainer = document.querySelector('.slider-container');
+    var _slider = _sliderContainer.querySelector('.slider');
     var _slides = _slider.querySelectorAll('.slide');
-    var _previousSlide = _sliderContainer.querySelector( 'button[name=previous_slide]' );
-    var _nextSlide = _sliderContainer.querySelector( 'button[name=next_slide]' );
+    var _previousSlide = _sliderContainer.querySelector('button[name=previous_slide]');
+    var _nextSlide = _sliderContainer.querySelector('button[name=next_slide]');
 
     var _previous = function (elements) {
       var previous = [];
 
       elements.forEach( function (element) {
-        if ( element.previousElementSibling ) {
+        if (element.previousElementSibling) {
           previous.push(element.previousElementSibling);
         }
       });
 
-      if ( previous.length > 0 ) {
+      if (previous.length > 0) {
+        console.log(previous);
         return previous;
       } else {
-        return [ _slides.slice( -1 )];
+        var s = [_slides].slice(-1);
+        console.log(s);
+        return s;
       }
     }
 
-    var _next = function ( elements ) {
+    var _next = function (elements) {
       var next = [];
 
-      elements.forEach( function ( element ) {
-        if ( element.nextElementSibling ) {
-          next.push( element.nextElementSibling );
+      elements.forEach( function (element) {
+        if (element.nextElementSibling) {
+          next.push(element.nextElementSibling);
         }
       });
 
-      if ( next.length > 0 ) {
+      if (next.length > 0) {
         return next;
       } else {
-        return [_slides[ 0 ]];
+        return [_slides[0]];
       }
     };
 
-    var _slide = function ( e ) {
+    var _slide = function (e) {
       e.preventDefault();
 
-      var slides = Array.prototype.slice.call( _slider.querySelectorAll( '.slide.is-reference' ));
-      var newSlides = _next( slides );
+      var slides = Array.prototype.slice.call( _slider.querySelectorAll('.slide.is-reference'));
+      var newSlides;
       var length = _slides.length;
 
-      slides.forEach( function ( element ) {
-        element.classList.remove( 'is-reference' );
+      slides.forEach( function (element) {
+        element.classList.remove('is-reference');
       });
 
-      // TODO: Fix this...
-      // if ( e.currentTarget.classList.contains( 'next' ) ) {
-      //   newSlides = _next( newSlides );
-      //   _slider.classList.remove( 'is-reversing' );
-      // } else {
-      //   newSlides = _next( newSlides );
-      //   _slider.classList.add( 'is-reversing' );
-      // }
+      if ( e.currentTarget.classList.contains('next') ) {
+        newSlides = _next(slides);
+        _slider.classList.remove('is-reversing');
+      } else {
+        newSlides = _previous(slides);
+        _slider.classList.add('is-reversing');
+      }
 
-      newSlides.forEach(function( element ) {
-        element.classList.add( 'is-reference' );
+      newSlides.forEach(function(element) {
+        element.classList.add('is-reference');
         element.style.order = 1;
       });
 
-      for ( var i = 2; i <= length; i++ ) {
+      for (var i = 2; i <= length; i++) {
         newSlides = _next( newSlides );
-        newSlides.forEach( function( element ) {
+        newSlides.forEach(function(element) {
           element.style.order = i;
         });
       }
 
-      _slider.classList.remove( 'is-set' );
+      _slider.classList.remove('is-set');
       return setTimeout(function () {
         return _slider.classList.add('is-set');
       }, 50);
     };
 
-    _previousSlide.addEventListener( 'click', _slide, false );
+    _previousSlide.addEventListener('click', _slide, false);
 
-    _nextSlide.addEventListener( 'click', _slide, false );
+    _nextSlide.addEventListener('click', _slide, false);
   });
 
 }(document));
